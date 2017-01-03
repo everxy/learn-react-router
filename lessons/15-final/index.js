bc75325122ca781515da13e088f70e0d07c0c340
Xyasq001 <xiongyao001@foxmail.com>
2017-01-03 20:56:03 +0800

    add lesson15 as a note

--- lessons/15-final/index.js
+++ lessons/15-final/index.js
@@ -6,6 +6,10 @@ import App from './modules/App'
 import repos from './modules/repos'
 import about from './modules/about'
 
+
+// lesson2 增加router，根节点变为Router
+// history：实际使用中应该使用 browerHistory，但是这个需要后台服务器有准备处理URL
+// 这里使用hashHistory更适合学习时使用
 render((
 <Router history={hashHistory}>
     <Route path="/" component={App} />
---
+++ lessons/15-final/modules/repos.js
@@ -0,0 +1,10 @@
+// lesson2 add
+import React from "react"
+
+export default React.createClass({
+    render(){
+        return <div>
+            repos
+        </div>  
+    }
+});
