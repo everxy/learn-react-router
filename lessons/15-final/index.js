06cb7413b37d03a5bed0705543ce172a77d7e612
Xyasq001 <xiongyao001@foxmail.com>
2017-01-04 13:26:02 +0800

    until lesson5 Link

--- lessons/15-final/index.js
+++ lessons/15-final/index.js
@@ -8,12 +8,22 @@ import about from './modules/about'
 
 
 // lesson2 增加router，根节点变为Router
+// 通过lesson2，实现了最基本的页面跳转
 // history：实际使用中应该使用 browerHistory，但是这个需要后台服务器有准备处理URL
 // 这里使用hashHistory更适合学习时使用
 render((
+// <Router history={hashHistory}>
+//  <Route path="/" component={App} />
+//      <Route path="/repos" component={repos} />
+//      <Route path="/about" component={about} />
+
+// lesson4 嵌套路由，让路由嵌套，这样就可以复用我们的导航栏，而不必在每个组件页面去渲染它
+// 首先让分支作为根节点的子节点
+// 然后进入App.js进一步调整
 <Router history={hashHistory}>
-    <Route path="/" component={App} />
-    <Route path="/repos" component={repos} />
-    <Route path="/about" component={about} />
+    <Route path="/" component={App} >
+        <Route path="/repos" component={repos} />
+        <Route path="/about" component={about} />
+    </Route>
 </Router>
 ), document.getElementById('app'))
--- lessons/15-final/modules/App.js
+++ lessons/15-final/modules/App.js
@@ -1,7 +1,27 @@
 import React from 'react'
+// lesson3 增加Link,可以在App组件里面点击链接跳转到另外的组件中了
+import { Link } from 'react-router'
+import { NavLink } from './NavLink'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (<div>
+      <h1>React router tutorial</h1>
+      <ul role="nav">
+        {/* Link 和 a标签的一个区别在于：Link可以连接是否激活并设置其style*/}
+        {/* 注意这里要使用两个{}将变量包含*/}
+        <li><Link to="/about" activeStyle={{ color: 'red' }}>about</Link></li>
+        {/* 或者添加类名替代内联样式 */}
+        <li><Link to="/repos" activeClassName="active">repos</Link></li>
+        
+        {/* 将连接封装为一个组件，这样在就可以复用，并且便于管理样式 */}
+        <li><NavLink to="/repos">repos</NavLink></li>
+     
+      </ul>
+
+      {/* lesson4 add this*/}
+      {/* 在这里就可以直接渲染出属于App组件子路由下的其他组件*/}
+      {this.props.children}
+    </div>)
   }
 })
